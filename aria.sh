3aff83284fdcceb2bab736f1a0a77f310069bad9
Ciara <64529639+Ciara-Aa@users.noreply.github.com>
2021-10-27 16:50:21 +0700

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -2,7 +2,7 @@ tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListColle
 aria2c --enable-rpc=true --check-certificate=false --daemon=true \
    --max-connection-per-server=10 --rpc-max-request-size=1024M --bt-max-peers=0 \
    --bt-stop-timeout=0 --min-split-size=10M --split=10 --allow-overwrite=true \
-   --max-overall-download-limit=0 --bt-tracker="[$tracker_list]" --disk-cache=64M \
+   --max-overall-download-limit=0 --bt-tracker="[$tracker_list]" --disk-cache=36M \
    --max-overall-upload-limit=0 --max-concurrent-downloads=99 --continue=true \
    --peer-id-prefix=-qB4380- --user-agent=qBittorrent/4.3.8 --peer-agent=qBittorrent/4.3.8 \
    --bt-enable-lpd=true --seed-time=0 --max-file-not-found=0 --max-tries=20 \
